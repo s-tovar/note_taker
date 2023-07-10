4f0dacc3d439818e4409c64e6be0f00349e233d0
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-07-10 14:25:40 -0700

    initialzed express.js and set a port to server.js file

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 // const { v4: uuidv4 } = require('uuid'); this generates unique ids for notes?
+
+const app = express();
+const PORT = process.env.PORT || 3000;
