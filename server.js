bd3365b3499b251f38fd0580f1e16f84ffc4daf2
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-07-10 14:48:57 -0700

    modified server.js by adding middleware and routes to notes.html, to read db.json, and receive new notes, and save it

--- server.js
+++ server.js
@@ -5,3 +5,36 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+//middleware to parse
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public')); 
+
+// route to notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+
+  // route to read db,json file 7 retuyrn saved notes as json when api/notes is accessed 
+  app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+  });
+
+  // routes to recieve a new note, save it and return new note 
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4(); // install uuid 
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const allNotes = JSON.parse(data);
+        allNotes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(allNotes, null, 2), (err) => {
+            if (err) throw err;
+            res.json(allNotes);
+        });
+    });
+  });
